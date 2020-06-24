47d5855e5db01f663fb053c649d82bbc02d3b848
kyungsoo.lee <lkseoqkr@gmail.com>
2020-06-24 22:16:47 +0900

    Updates

---
+++ precache-manifest.186c2e35f625f8972becc6d85f562305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc4e241d31f51d3d3138a6c2367d7b3",
+    "url": "/ghpagetestrepo/index.html"
+  },
+  {
+    "revision": "43b3928252fab64c68af",
+    "url": "/ghpagetestrepo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "583d733a2b3a0d2cf927",
+    "url": "/ghpagetestrepo/static/js/2.f0107553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpagetestrepo/static/js/2.f0107553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b3928252fab64c68af",
+    "url": "/ghpagetestrepo/static/js/main.761e9419.chunk.js"
+  },
+  {
+    "revision": "59b199331fafcc6f84fa",
+    "url": "/ghpagetestrepo/static/js/runtime-main.a690552d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpagetestrepo/static/media/logo.5d5d9eef.svg"
+  }
+]);
